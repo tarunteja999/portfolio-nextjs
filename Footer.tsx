968a050dfce9eef8efbb1cbad8314b1c7c5071ca
Tarun Teja Akkipalli <taruntejaedu@gmail.com>
2025-09-09 02:54:58 -0400

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-white/10 mt-16">
-      <div className="container py-6 text-xs text-gray-400">
-        © {new Date().getFullYear()} Tarun Teja Akkipalli. All rights reserved.
-      </div>
-    </footer>
-  )
-}
